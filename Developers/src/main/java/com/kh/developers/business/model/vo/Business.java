bb165b82e1784cc6210e37bf7b0627da3f4542b9
paulwoojungkim <wj2kim@gmail.com>
2019-10-25 14:00:42 +0900

    business vo 작성 재커밋.

---
+++ Developers/src/main/java/com/kh/developers/business/model/vo/Business.java
@@ -0,0 +1,27 @@
+package com.kh.developers.business.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Business {
+
+	private String busName;
+	private String busCountry;
+	private String busArea;
+	private String busAddress;
+	private String busRegNum;
+	private String busIncome;
+	private String busIndustrial;
+	private String busTotalEmp;
+	private String busIntroduce; 
+	private String busEstablishment;
+	private String busEmail;
+	private String busPhone;
+	private String busWebsite;
+	private String searchKeywords; 
+	private String[] regPath;
+}
