ca14b4ce6d4cfb6332287e1e8052ac91053a489a
ljs614 <sjl0614@naver.com>
2019-11-06 16:55:32 +0900

    commit applicant vo 추가

---
+++ Developers/src/main/java/com/kh/developers/business/model/vo/Applicant.java
@@ -0,0 +1,21 @@
+package com.kh.developers.business.model.vo;
+
+import java.sql.Date;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Applicant {
+
+	private int busNo;
+	private int memNo;
+	private int positionNo;
+	private int resumeNo;
+	private int applStatus;
+	private Date applDate;
+	private char applAnsYn;
+}
