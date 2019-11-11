5b02fed827145398d37afc06679af91a8ba27450
ljs614 <sjl0614@naver.com>
2019-11-11 21:17:33 +0900

    commit 비지니스 컨트롤러2 에러 해결

--- Developers/src/main/java/com/kh/developers/business/model/service/BusinessServiceImpl2.java
+++ Developers/src/main/java/com/kh/developers/business/model/service/BusinessServiceImpl2.java
@@ -23,21 +23,18 @@ public class BusinessServiceImpl2 implements BusinessService2 {
 	@Override
 	public int updateBusInfo(Business bus) {
 		int result=dao.updateBusInfo(session, bus);
-		session.close();
 		return result;
 	}
 
 	@Override
 	public int busLogoChange(Business busInfo) {
 		int result=dao.busLogoChange(session, busInfo);
-		session.close();
 		return result;
 	}
 
 	@Override
 	public int busImgAdd(Map map) {
 		int result=dao.busImgAdd(session, map);
-		session.close();
 		return result;
 	}
 
@@ -45,35 +42,30 @@ public class BusinessServiceImpl2 implements BusinessService2 {
 	@Override
 	public Business selectBusOne(String busNo) {
 		Business bus=dao.selectBusOne(session, busNo);
-		session.close();
 		return bus;
 	}
 
 	@Override
 	public List<Applicant> selectBusAppl(Map map, int cPage, int numPerPage) {
 		List<Applicant> list=dao.selectBusAppl(session, map, cPage, numPerPage);
-		session.close();
 		return list;
 	}
 
 	@Override
 	public int selectBusApplCount(Map map) {
 		int result=dao.selecBusApplCount(session, map);
-		session.close();
 		return result;
 	}
 	
 	@Override
 	public int insertApplicant(Map map) {
 		int result=dao.insertApplicant(session, map);
-		session.close();
 		return result;
 	}
 
 	@Override
 	public int deleteApplicant(Map map) {
 		int result=dao.deleteApplicant(session, map);
-		session.close();
 		return result;
 	}
 	
@@ -84,7 +76,6 @@ public class BusinessServiceImpl2 implements BusinessService2 {
 	@Override
 	public int insertApplLike(Map map) {
 		int result=dao.insertApplLike(session, map);
-		session.close();
 		return result;
 	}
 	
@@ -92,7 +83,6 @@ public class BusinessServiceImpl2 implements BusinessService2 {
 	@Override
 	public int deleteApplLike(Map map) {
 		int result=dao.deleteApplLike(session, map);
-		session.close();
 		return result;
 	}
 	
@@ -101,14 +91,12 @@ public class BusinessServiceImpl2 implements BusinessService2 {
 	@Override
 	public int selectCheckLike(Map map) {
 		int result=dao.selectCheckLike(session, map);
-		session.close();
 		return result;
 	}
 
 	@Override
 	public Member selectApplicant(int memNo) {
 		Member m=dao.selectApplicant(session, memNo);
-		session.close();
 		return m;
 	}
 	
