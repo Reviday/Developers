bd24c8669b1b668902b1767eaa5c61fdf663ea96
kiyoungsung <ysung26@daum.net>
2019-11-18 22:52:09 +0900

    객체추가

---
+++ Developers/src/main/java/com/kh/developers/search/model/vo/AdCompany.java
@@ -0,0 +1,17 @@
+package com.kh.developers.search.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AdCompany {
+
+	private int bus_no;
+	private String bus_logo;
+	private String[] bus_images;
+	private int po_count;
+	
+}
