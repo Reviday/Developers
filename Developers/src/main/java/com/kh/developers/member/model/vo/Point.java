940fa95c4de79d7d6fea7371f6b1fb6edbe3631e
kang-15 <cay9506@naver.com>
2019-11-18 21:49:01 +0900

    객체 설명 추가

--- Developers/src/main/java/com/kh/developers/member/model/vo/Point.java
+++ Developers/src/main/java/com/kh/developers/member/model/vo/Point.java
@@ -10,10 +10,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @Data
 public class Point {
-	private int pointNo;
-	private int memNo;
-	private int memPointPlus;
-	private String pointIntro;
-	private Date pointDate;
-	private int memPoint;
+	private int pointNo;//포인트 시퀀스
+	private int memNo; //포인트 줄멤버 넘버
+	private int memPointPlus; //멤버에게 줄 포인트
+	private String pointIntro; //무슨 이유로 주는지 소개
+	private Date pointDate; //포인트 준 날짜
+	private int memPoint; //멤버 포인트 가져올 용도 
 }
